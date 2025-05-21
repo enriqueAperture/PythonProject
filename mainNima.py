8808026f2ec9e7e13bd75b46cdf8091a0bdf29fc
AlexMetalls <ahuarachi@metallsdelcampslu.es>
2025-05-21 08:19:20 +0200

    mainNima incorporado

--- mainNima.py
+++ mainNima.py
@@ -15,8 +15,8 @@ import nimaFunctions
 
 NIF_PRUEBA = "B43693274"
 
-#nimaFunctions.busqueda_NIMA_Valencia(NIF_PRUEBA)
-#nimaFunctions.busqueda_NIMA_Madrid(NIF_MADRID)
+#datos_json = nimaFunctions.busqueda_NIMA_Valencia(NIF_PRUEBA)
+#datos_json = nimaFunctions.busqueda_NIMA_Madrid(NIF_MADRID)
 datos_json = nimaFunctions.busqueda_NIMA_Castilla(NIF_PRUEBA)
 
 print(datos_json)
